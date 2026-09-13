0d4376ab86d0b15718dfbaa3caa8cfc382aed95b
mahmud08316 <nizomovm856@gmail.com>
2026-09-13 14:40:52 +0500

    Update test.py

--- test.py
+++ test.py
@@ -1,15 +1,31 @@
 from flask import Flask, request
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask("ChatApp")
 
-xabarlar = []
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///chat.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db = SQLAlchemy(app)
+
+
+class Message(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.String(500), nullable=False)
+
+
+with app.app_context():
+    db.create_all()
+
 
 @app.route("/")
 def home():
+    messages = Message.query.all()
+
     xabarlar_html = ""
 
-    for xabar in xabarlar:
-        xabarlar_html += f"<p>💬 {xabar}</p>"
+    for message in messages:
+        xabarlar_html += f"<p>💬 {message.text}</p>"
 
     return f"""
     <h1>ChatApp 💬</h1>
@@ -22,13 +38,17 @@ def home():
     </form>
     """
 
+
 @app.route("/send", methods=["POST"])
 def send():
     message = request.form.get("message")
 
     if message:
-        xabarlar.append(message)
+        yangi_xabar = Message(text=message)
+        db.session.add(yangi_xabar)
+        db.session.commit()
 
     return home()
+
 
 app.run(host="0.0.0.0", port=5000)
