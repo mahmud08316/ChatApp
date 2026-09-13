1d07b883c465471b3b36dd034429e84406e06555
mahmud08316 <nizomovm856@gmail.com>
2026-09-13 14:13:39 +0500

    Update test.py

--- test.py
+++ test.py
@@ -1,9 +1,21 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask("ChatApp")
 
 @app.route("/")
 def home():
-    return "ChatApp server ishlayapti!"
+    return """
+    <h1>ChatApp 💬</h1>
+
+    <form action="/send" method="post">
+        <input name="message" placeholder="Xabar yozing">
+        <button type="submit">Yuborish</button>
+    </form>
+    """
+
+@app.route("/send", methods=["POST"])
+def send():
+    message = request.form.get("message")
+    return f"<h1>Siz yozdingiz:</h1><p>{message}</p>"
 
 app.run(host="0.0.0.0", port=5000)
