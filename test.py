d17d5e89caccf90bfddbbb0574c1db45bd574a60
mahmud08316 <nizomovm856@gmail.com>
2026-09-13 14:31:14 +0500

    Update test.py

--- test.py
+++ test.py
@@ -2,10 +2,19 @@ from flask import Flask, request
 
 app = Flask("ChatApp")
 
+xabarlar = []
+
 @app.route("/")
 def home():
-    return """
+    xabarlar_html = ""
+
+    for xabar in xabarlar:
+        xabarlar_html += f"<p>💬 {xabar}</p>"
+
+    return f"""
     <h1>ChatApp 💬</h1>
+
+    {xabarlar_html}
 
     <form action="/send" method="post">
         <input name="message" placeholder="Xabar yozing">
@@ -16,6 +25,10 @@ def home():
 @app.route("/send", methods=["POST"])
 def send():
     message = request.form.get("message")
-    return f"<h1>Siz yozdingiz:</h1><p>{message}</p>"
+
+    if message:
+        xabarlar.append(message)
+
+    return home()
 
 app.run(host="0.0.0.0", port=5000)
